dc6f0973afe92d00f32a2abd2154f1f908dffb5e
hosein pourtaghi <54171194+hosein-pourtaghi@users.noreply.github.com>
2025-01-31 06:47:22 +0000

    add template

--- ExcelGenerator/Program.cs
+++ ExcelGenerator/Program.cs
@@ -9,14 +9,16 @@ namespace ExcelGenerator
     {
         static void Main(string[] args)
         {
-var students = GetStudentList().OrderBy(s => s.Name).ToList(); // Fetch and sort students by name
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // Set the license context
 
-            var templatePath = "path/to/your/template.xlsx"; // Path to your Excel file A
-            var outputPath = "path/to/output/fileB.xlsx"; // Path to save Excel file B
+            var students = GetStudentList().OrderBy(s => s.Name).ToList(); // Fetch and sort students by name
+
+            var templatePath = "./template.xlsx"; // Path to your Excel file A
+            var outputPath = "./fileB.xlsx"; // Path to save Excel file B
 
             using (var templatePackage = new ExcelPackage(new FileInfo(templatePath)))
-            {
-                var templateWorksheet = templatePackage.Workbook.Worksheets[0];
+            {templatePackage.Workbook.Worksheets
+                var templateWorksheet = [0];
 
                 // Create a new Excel package for output
                 using (var outputPackage = new ExcelPackage())
